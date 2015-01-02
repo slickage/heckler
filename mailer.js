f5f7e5cb70a770b1c2cd0122bc8a3b66ea19ce6b
James Wang <jw@slickage.com>
2015-01-01 20:36:13 -1000

    Added smtp transport in lieu of old unsupported config.

--- mailer.js
+++ mailer.js
@@ -1,18 +1,19 @@
 var nodemailer = require('nodemailer');
-var ses = require('nodemailer-ses-transport');
+var smtpTransport = require('nodemailer-smtp-transport');
 var config = require('./config');
-var smtpTransport = nodemailer.createTransport('SMTP',{
+
+var transport = nodemailer.createTransport(smtpTransport({
   host: config.smtp.host,
-  secureConnection: true, // use SSL
   port: config.smtp.port,
+  secure: true,
   auth: {
     user: config.smtp.user,
     pass: config.smtp.pass
   }
-});
+}));
 
 module.exports = {
   email: function(email, cb) {
-    smtpTransport.sendMail(email, cb);
+    transport.sendMail(email, cb);
   }
 }
