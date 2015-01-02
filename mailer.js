8e86566dca595204b1c51452ea6b29f1b5c38d46
James Wang <jw@slickage.com>
2015-01-01 19:11:56 -1000

    Nodemailer using ses transport + 1.3.0

--- mailer.js
+++ mailer.js
@@ -1,18 +1,14 @@
 var nodemailer = require('nodemailer');
+var ses = require('nodemailer-ses-transport');
 var config = require('./config');
-var smtpTransport = nodemailer.createTransport('SMTP',{
-  host: config.smtp.host,
-  secureConnection: true, // use SSL
-  port: config.smtp.port,
-  auth: {
-    user: config.smtp.user,
-    pass: config.smtp.pass
-  }
-});
+var transport = nodemailer.createTransport(ses({
+  accessKeyId: config.smtp.user,
+  secretAccessKey: config.smtp.pass
+}));
 
 module.exports = {
   email: function(email, cb) {
-    smtpTransport.sendMail(email, cb);
+    transport.sendMail(email, cb);
   }
 }
 
